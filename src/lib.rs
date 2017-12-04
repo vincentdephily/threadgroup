098d5bc6a56d8205a645fd3123f8cf6b4ba5c754
Vincent de Phily <vincent.dephily@mobile-devices.fr>
2017-12-04 15:34:39 +0100

    Doc fixes, clarifications, and links.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-//! Manages a group of threads that all have the same return type and can be join()ed as a unit.
+//! Manages a group of threads that all have the same return type and can be [join()]ed as a unit.
 //!
-//! The implementation uses a mpsc channel internally so that children (spawned threads) can notify the parent
-//! (owner of a `ThreadGroup`) that they are finished without the parent having to use a blocking
-//! `std::thread::JoinHandle.join()` call.
+//! The implementation uses a [mpsc channel] internaly so that children (spawned threads) can notify
+//! the parent (owner of a [`ThreadGroup`]) that they are finished without the parent having to use
+//! a blocking [`std::thread::JoinHandle.join()`] call.
 //!
 //! # Examples
 //! ```rust
@@ -25,6 +25,12 @@
 //! assert_eq!(2,                   tg.join().unwrap());
 //! assert_eq!(JoinError::Timeout,  tg.join_timeout(Duration::new(0,10000)).unwrap_err());
 //! ```
+//! [join()]:                           struct.ThreadGroup.html#method.join
+//! [`ThreadGroup`]:                    struct.ThreadGroup.html
+//! [mpsc channel]:                     https://doc.rust-lang.org/stable/std/sync/mpsc/index.html
+//! [`std::thread::JoinHandle.join()`]: https://doc.rust-lang.org/stable/std/thread/struct.JoinHandle.html#method.join
+//#![doc(html_playground_url = "https://play.rust-lang.org/")]
+//#![doc(include = "README.md")]
 
 use std::sync::mpsc;
 use std::sync::mpsc::{Receiver, RecvError, RecvTimeoutError, Sender};
@@ -32,7 +38,9 @@ use std::thread;
 use std::thread::{JoinHandle, ThreadId};
 use std::time::Duration;
 
-/// Possible error returns from `join()` and `join_timeout()`.
+/// Possible error returns from [`join()`] and [`join_timeout()`].
+/// [`join()`]:         struct.ThreadGroup.html#method.join
+/// [`join_timeout()`]: struct.ThreadGroup.html#method.join_timeout
 #[derive(Debug,PartialEq)]
 pub enum JoinError {
     /// Thread list is empty, nothing to join.
@@ -45,7 +53,7 @@ pub enum JoinError {
     Disconnected,
 }
 
-/// Holds the vector of threads and the notification channel.
+/// Holds the collection of threads and the notification channel.
 /// All public functions operate on this struct.
 pub struct ThreadGroup<T> {
     tx: Sender<ThreadId>,
@@ -53,7 +61,7 @@ pub struct ThreadGroup<T> {
     handles: Vec<JoinHandle<T>>,
 }
 
-/// Sends current thread id on the channel when this struct gets out of scope.
+/// Sends current thread id on its channel when it gets out of scope.
 struct SendOnDrop {
     tx: Sender<ThreadId>,
 }
@@ -80,7 +88,9 @@ impl<T> ThreadGroup<T> {
         ThreadGroup::<T>{tx: tx, rx: rx, handles: vec![]}
     }
 
-    /// Spawn a new thread (like `std::thread::join()`) in this thread group.
+    /// Spawn a new thread, adding it to the ThreadGroup.
+    ///
+    /// Operates like [`std::thread::spawn()`], but modifies the ThreadGroup instead of returning a [`JoinHandle`].
     /// # Examples
     /// ```rust
     /// use std::time::Duration;
@@ -89,6 +99,8 @@ impl<T> ThreadGroup<T> {
     /// let mut tg: ThreadGroup<u32> = ThreadGroup::new();
     /// tg.spawn::<_,u32>(|| {sleep(Duration::new(0,1000000));1});
     /// ```
+    /// [`std::thread::spawn()`]: https://doc.rust-lang.org/stable/std/thread/fn.spawn.html
+    /// [`JoinHandle`]:           https://doc.rust-lang.org/stable/std/thread/struct.JoinHandle.html
     // FIXME: Is there a way to remove the need to specify the type when calling spawn() ?
     //        The ThreadGroup has type T and we know that R is the same type, but the compiler doesn't see that.
     pub fn spawn<F, R>(&mut self, f: F)
@@ -106,19 +118,23 @@ impl<T> ThreadGroup<T> {
         self.handles.push(jh);
     }
 
-    /// Return count of threads that have been `spawn()`ed but not yet `join()`ed.
+    /// Return count of threads that have been [`spawn()`]ed but not yet [`join()`]ed.
+    /// [`spawn()`]: struct.ThreadGroup.html#method.spawn
+    /// [`join()`]: struct.ThreadGroup.html#method.join
     pub fn len(&self) -> usize {
         self.handles.len()
     }
 
-    /// Check if there is any thread not yet `join()`ed.
+    /// Check if there is any thread not yet [`join()`]ed.
+    /// [`join()`]: struct.ThreadGroup.html#method.join
     pub fn is_empty(&self) -> bool {
         self.handles.is_empty()
     }
 
     /// Join one thread of the ThreadGroup.
     ///
-    /// The thread will be joined in termination order, not creation order.
+    /// Operates like [`std::thread::JoinHandle.join()`], but picks the first thread that
+    /// terminates.
     /// # Examples
     /// ```rust
     /// use threadgroup::ThreadGroup;
@@ -130,6 +146,7 @@ impl<T> ThreadGroup<T> {
     ///     }
     /// }
     /// ```
+    /// [`std::thread::JoinHandle.join()`]: https://doc.rust-lang.org/stable/std/thread/struct.JoinHandle.html#method.join
     pub fn join(&mut self) -> Result<T, JoinError> {
         match self.handles.is_empty() {
             true => Err(JoinError::AllDone),
@@ -140,7 +157,10 @@ impl<T> ThreadGroup<T> {
         }
     }
 
-    /// Try to join one thread of the ThreadGroup, and give up after a timeout.
+    /// Try to join one thread of the ThreadGroup.
+    ///
+    /// Operates like [`std::thread::JoinHandle.join()`], but picks the first thread that terminates
+    /// and gives up if the timeout is reached.
     /// # Examples
     /// ```rust
     /// use std::time::Duration;
@@ -152,6 +172,7 @@ impl<T> ThreadGroup<T> {
     ///    }
     /// }
     /// ```
+    /// [`std::thread::JoinHandle.join()`]: https://doc.rust-lang.org/stable/std/thread/struct.JoinHandle.html#method.join
     pub fn join_timeout(&mut self, timeout: Duration) -> Result<T, JoinError> {
         match self.handles.is_empty() {
             true => Err(JoinError::AllDone),
@@ -163,7 +184,7 @@ impl<T> ThreadGroup<T> {
         }
     }
 
-    /// Find a thread by its id
+    /// Find a thread by its id.
     // TODO: replace with https://doc.rust-lang.org/nightly/std/vec/struct.Vec.html#method.remove_item
     fn find(&self, id: ThreadId) -> Option<usize> {
         for (i,jh) in self.handles.iter().enumerate() {
@@ -174,7 +195,8 @@ impl<T> ThreadGroup<T> {
         None
     }
 
-    /// Actual thread::join() once we know that the thread has finished
+    /// Actual [`JoinHandle.join()`] once we know that the thread has finished.
+    /// [`JoinHandle.join()`]: https://doc.rust-lang.org/stable/std/thread/struct.JoinHandle.html#method.join
     fn do_join(&mut self, id: ThreadId) -> Result<T, JoinError> {
         // We need to separately find and remove the JoinHandle from the vector in order to not upset the borrow checker
         let i = self.find(id).unwrap();
